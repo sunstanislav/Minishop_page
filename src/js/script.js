c19147dd141f9566548803cb0daefcb49b729209
Stanislav <sundeev91@ya.ru>
2021-09-16 15:17:54 +0600

    add slider

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        dotsClass: 'slick-dots',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
