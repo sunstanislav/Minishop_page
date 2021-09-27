c584140dffbeb3e2e2038873ecc212ccde09aaa3
Stanislav <sundeev91@ya.ru>
2021-09-27 17:49:36 +0600

    add script tabs

--- src/js/script.js
+++ src/js/script.js
@@ -26,11 +26,6 @@ const slider = tns({
     navPosition: 'bottom',
     controls: false,
     nav: true
-    // responsive: {
-    //     320: {
-    //       nav: false
-    //     }
-    //   }
   });
   
   document.querySelector('.prev').addEventListener ('click', function () {
@@ -39,3 +34,9 @@ const slider = tns({
   document.querySelector('.next').addEventListener ('click', function () {
     slider.goTo('next');
   })
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
