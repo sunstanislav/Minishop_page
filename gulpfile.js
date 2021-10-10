9b4f8702cd18d530d48b60a6b4775c1352c7e928
sunstanislav <sundeev91@ya.ru>
2021-10-10 22:59:10 +0600

    add deploy

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ const deploy = require("gulp-gh-pages");
 /**
  * Push build to gh-pages
  */
-gulp.task("deploy", function () {
+ gulp.task("deploy", function () {
 	return gulp.src("./dist/**/*").pipe(deploy());
 });
 
